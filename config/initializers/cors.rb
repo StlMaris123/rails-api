d33ddddb58d89d9169b7730d5512b8c8ea4d70ab
StlMaris123 <stellamarisnjage@gmail.com>
2018-01-17 21:15:26 +0300

    set up cors resources

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,13 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+cors_origins = ( ENV['CORS_ORIGINS'] ? Regexp.new(ENV['CORS_ORIGINS']) : '*' )
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins cors_origins
+    resource '*'
+    headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
