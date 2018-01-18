fb788062e0e95f469d3b5b2675d4e6ab69d3bb98
StlMaris123 <stellamarisnjage@gmail.com>
2018-01-18 15:08:48 +0300

    add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :card
-  belongs_to :creator
+  belongs_to :creator, class_name: 'User'
+
+  validates :body, presence: true
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,8 @@
 class List < ApplicationRecord
   belongs_to :board
-  belongs_to :creator
+  belongs_to :creator, class_name: 'user'
+
+  has_many :cards, dependent: :destroy
+
+  validates :title, presence: true
 end
