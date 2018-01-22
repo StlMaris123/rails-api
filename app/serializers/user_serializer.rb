54e87014340acb946a42079f30d88de5f8bef23b
StlMaris123 <stellamarisnjage@gmail.com>
2018-01-22 20:51:34 +0300

    access model being serialized via the object accessor

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :gravatar_url, :admin, :created_at, :updated_at
+
+  attributes :mailto_url
+
+  def mailto_url
+    "mailto:#{ object.email}"
+  end
 end
