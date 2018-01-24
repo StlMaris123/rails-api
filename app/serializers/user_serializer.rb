98f9585f3046e0e91ba4c2df3bd4efd4db5f887b
StlMaris123 <stellamarisnjage@gmail.com>
2018-01-24 05:53:07 +0300

    include rails url helper mixins to format any links using routing helpers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+
   attributes :id, :email, :gravatar_url, :admin, :created_at, :updated_at
 
   attributes :mailto_url
@@ -6,4 +8,16 @@ class UserSerializer < ActiveModel::Serializer
   def mailto_url
     "mailto:#{ object.email}"
   end
+
+  def link(rel, href)
+    { rel: rel, href: href }
+  end
+
+  def links
+    [
+      link(:self, user_url(object)),
+      link(:boards, user_boards_url(object))
+    ]
+  end
+
 end
