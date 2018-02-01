e85957d7ee5bbe353a82f62666792062f1732973
StlMaris123 <stellamarisnjage@gmail.com>
2018-02-01 06:29:12 +0300

    cudtomize the response delivered to rate limited clients

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,3 +1,24 @@
-Rack::Attack.throttle("req/ip", :limit => 10, :periood => 1.second) do |req|
-  req.ip
-end
+Rack::Attack.throttled_response = ->(env) {
+  status_code = 429
+  now = Time.now
+  match_data = env['rack.attack.,atch_data'] || {}
+  retry_after = match_data[:period] - now.to_i % match_data[:period]
+
+  headers = {
+    'Content-Type' => 'application/json',
+    'Retry-After'  => retry_after.to_js,
+    'X-RateLimit-Limit' => match_data[:limit].to_s,
+    'X-RateLimit-Remaining' => '0',
+    'X-RateLimit-Reset' => (now + retry_after).to_s
+  }
+
+  body = {
+    error: {
+      status: status_code,
+      name: "Too many Requests",
+      message: "Throttle Limit Reached. Retry later"
+    }
+  }
+  [status_code, headers, [body.to_json]]
+}
+
